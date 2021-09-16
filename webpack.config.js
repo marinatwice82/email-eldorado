d3e92204c03c97ecd656f5799a44b3dd12586385
Marina <marinatwice82@gmail.com>
2021-09-16 13:59:03 +0300

    load files

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+module.exports = {
+  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+  
+
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: '[name].js', // Name of generated bundle after build
+    publicPath: '/' // public URL of the output directory when referenced in a browser
+  },
+  module: {  // where we defined file patterns and their loaders
+      rules: [ 
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: [
+              /node_modules/
+            ]
+          },
+          {
+            test: /\.(less|lcss)$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource'
+          }
+      ]
+  },
+  plugins: [  // Array of plugins to apply to build chunk
+      new HtmlWebpackPlugin({
+          template: __dirname + "/src/public/index.html",
+          inject: 'body'
+      })
+  ],
+  devServer: {  // configuration for webpack-dev-server
+      contentBase: './src/public',  //source of static assets
+      port: 7700, // port to run dev-server
+  }
+};
